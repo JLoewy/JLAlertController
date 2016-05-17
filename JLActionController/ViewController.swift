feaa54567d694609bb9c3ca43270a26014f60cf1
Jason Loewy <JasonLoewy@gmail.com>
2016-05-17 15:58:11 -0700

    Initial Commit

---
+++ JLActionController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLActionController
+//
+//  Created by Jason Loewy on 5/17/16.
+//  Copyright © 2016 Jason Loewy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
